eab4173d71ea038e1d87eff4edff39ca45cb3c74
jellyfish-p <jellyfish_p@vip.qq.com>
2026-07-02 16:45:49 +0800

    remove unused dict

--- nuxt-reelmind/server/api/models/lego.get.ts
+++
@@ -1,9 +0,0 @@
-import type { LegoModelResponse } from '../../../types/reelmind'
-
-export default defineEventHandler(async (event) => {
-  const authHeader = getHeader(event, 'authorization')
-  const token = authHeader?.replace('Bearer ', '')
-
-  const result = await nestGet<LegoModelResponse[]>('/lego/models', undefined, token)
-  return result
-})
--- nuxt-reelmind/server/plugins/init.ts
+++
@@ -1,6 +0,0 @@
-export default defineNitroPlugin(async () => {
-  const { initializeDatabase } = await import('../db/init')
-  const { startTokenPolling } = await import('../utils/token-manager')
-  await initializeDatabase()
-  await startTokenPolling()
-})
